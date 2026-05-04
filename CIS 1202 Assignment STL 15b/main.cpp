b9f90ed741d334ca1aa9b488e82e0e439c410c91
Carter Cox <cartertubergamer@gmail.com>
2026-05-03 22:08:46 -0400

    Initial Commit

---
+++ CIS 1202 Assignment STL 15b/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS 1202 Assignment STL 15b
+//
+//  Created by Carter Cox on 5/3/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
