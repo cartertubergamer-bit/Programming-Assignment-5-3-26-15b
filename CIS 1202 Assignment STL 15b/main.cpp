db784cef47b03728b88d57d1e56bc61f1229a863
Carter Cox <cartertubergamer@gmail.com>
2026-05-03 22:13:06 -0400

    Added templated half function for floating types

--- CIS 1202 Assignment STL 15b/main.cpp
+++ CIS 1202 Assignment STL 15b/main.cpp
@@ -3,6 +3,19 @@
 // CIS 1202 Assignment STL 15b
 
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <typename T>
+T half(T value) {
+    return value / 2;
+}
+
+// integer version with rounding rules
+int half(int value) {
+    return static_cast<int>(round(value / 2.0));
+}
 
 int main() {
     
